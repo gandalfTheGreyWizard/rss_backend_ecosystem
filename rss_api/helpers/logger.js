f80d022ae4e2c2bf97d93da2f5d93bc7a5f75833
gandalfTheGreyWizard <chakraborty.soumik23@gmail.com>
2025-11-11 14:13:53 +0000

    finalized logger implementation and update all console logs and errors to logger

--- rss_api/helpers/logger.js
+++ rss_api/helpers/logger.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 
 const winston = require('winston');
 const logger = winston.createLogger({
   transports: new winston.transports.Console(),
-  level: 'debug'
+  level: process.env.LOG_LEVEL
 });
 
 module.exports = logger;
