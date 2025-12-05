7f577dbbcfe2188f7cf6e492c871a052b82697a5
gandalfTheGreyWizard <chakraborty.soumik23@gmail.com>
2025-12-05 16:36:00 +0000

    - added docker file for building the application inside rss api - added docker compose in root directory for starting the application

--- rss_api/routes/auth.js
+++ rss_api/routes/auth.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const magicLinkStrategy = require('passport-magic-link').Strategy;
-const sendgrid = require('@sendgrid/mail');
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 const userModel = require('../models/user');
 const authController = require('../controllers/auth');
 
